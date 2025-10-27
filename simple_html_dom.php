cdb1299c67d9ed0317e2973ff17361f5f329441e
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2025-10-28 01:42:56 +0530

    Clean up comments in simple_html_dom.php
    Removed commented instructions for SourceForge users.

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -50,10 +50,7 @@ function file_get_html(
 		$defaultSpanText
 	);
 
-	/**
-	 * For sourceforge users: uncomment the next line and comment the
-	 * retrieve_url_contents line 2 lines down if it is not already done.
-	 */
+
 	$contents = file_get_contents(
 		$url,
 		$use_include_path,
